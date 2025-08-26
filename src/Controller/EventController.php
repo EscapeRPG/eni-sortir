d607934b8a47ec6f7abe02542e7e40e45246e7aa
EscapeRPG <syrphin@syrphin.com>
2025-08-26 15:52:39 +0200

    Task #0 - Added prefixed Routes

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Event;
-use App\Entity\Place;
 use App\Form\EventType;
 use App\Helper\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,11 +14,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
+#[Route('/event', name: 'event')]
 final class EventController extends AbstractController
 {
 
-    #[Route('/event/create', name: 'event_create')]
+    #[Route('/create', name: '_create')]
     public function create(Request $request, EntityManagerInterface $em, ParameterBagInterface $parameterBag, FileUploader $fileUploader): Response
     {
         $event = new Event();
@@ -35,12 +34,12 @@ final class EventController extends AbstractController
 
             $file = $form->get('poster_file')->getData();
             if ($file instanceof UploadedFile) {
-                $name = $fileUploader->upload($file,$event->getName(),$parameterBag->get('event')['poster_file']);
-                    $event->setPosterFile($name);
+                $name = $fileUploader->upload($file, $event->getName(), $parameterBag->get('event')['poster_file']);
+                $event->setPosterFile($name);
             }
 
             $interval = $start->diff($end);
-            $minutes = ($interval->days * 24 * 60) + ($interval->h * 60)+$interval->i;
+            $minutes = ($interval->days * 24 * 60) + ($interval->h * 60) + $interval->i;
             $event->setDuration($minutes);
 
             $user = $this->getUser();
@@ -58,28 +57,27 @@ final class EventController extends AbstractController
             return $this->redirectToRoute('app_main');
         }
         return $this->render('event/create.html.twig', [
-            'event_form'=>$form,
+            'event_form' => $form,
         ]);
     }
 
-    #[Route('/event/{id}/edit', name: 'event_edit', requirements : ['id'=> '\d+'])]
+    #[Route('/{id}/edit', name: '_edit', requirements: ['id' => '\d+'])]
     public function edit(Event $event, Request $request, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()){
+        if ($form->isSubmitted()) {
             $em->flush();
             $this->addFlash('success', 'Event edited!');
-            return $this->redirectToRoute('app_main', ['id'=>$event->getId()]);
+            return $this->redirectToRoute('app_main', ['id' => $event->getId()]);
         }
         return $this->render('event/create.html.twig', [
-            'event_form'=>$form,
+            'event_form' => $form,
         ]);
     }
+
     #[Route('/list{page}',
-
-
         name: '_list',
         requirements: ['page' => '\d+'],
         defaults: ['page' => 1])]
@@ -106,7 +104,8 @@ final class EventController extends AbstractController
     }
 
     #[Route('/detail/{id}', name: '_detail', requirements: ['id' => '\d+'])]
-    public function detail(SortieRepository $sortieRepository, int $id, ParameterBagInterface $bag): Response {
+    public function detail(SortieRepository $sortieRepository, int $id, ParameterBagInterface $bag): Response
+    {
         return $this->render('event/detail.html.twig', []);
     }
 
